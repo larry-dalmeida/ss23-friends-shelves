93f89049d0d05014a096c4b9f49255c1ba072237
esta3 <rauschert-e@web.de>
2023-08-23 19:58:52 +0200

    add all files

---
+++ models/book.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+// const Review = require('./review');
+const Schema = mongoose.Schema;
+
+const BookSchema = new Schema({
+    title: String,
+    author: String,
+    isbn: String,
+    image: String,
+    blurb: String,
+    // owner: {
+    //     type: Schema.Types.ObjectId,
+    //     ref: 'User',
+    // },
+});
+
+
+module.exports = mongoose.model('Book', BookSchema);
---
+++ seeds/seedHelpers.js
@@ -0,0 +1,48 @@
+
+module.exports.firstName = [
+    'Forest',
+    'Peter',
+    'Paul',
+    'Anna',
+    'Markus',
+    'Jack',
+    'Sarah',
+    'Jhon',
+]
+
+module.exports.lastName = [
+    'Miller',
+    'Lang',
+    'Smidt',
+    'Brown',
+    'Davis',
+    'Wilson',
+    'Jackson',
+    'Parker',
+]
+
+module.exports.titleStart = [
+    'Hand of',
+    'Primed for',
+    'King of',
+    'Devils',
+    'Clue of the',
+    'Sign of the',
+    'Years of',
+    'The Kingdom of',
+]
+
+
+
+module.exports.titleEnd = [
+    'Andromeda',
+    'Sin',
+    'Canyon',
+    'Pond',
+    'Aces',
+    'Heir',
+    'Ghost Town',
+    'Absent Staircase',
+    'Invisible Violin',
+    'Goodbyes',
+]
