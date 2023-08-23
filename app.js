c493e1b40544f62bbe61982625fdbdf861ebf70a
esta3 <rauschert-e@web.de>
2023-08-23 19:58:52 +0200

    add all files

---
+++ app.js
@@ -0,0 +1,108 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
+const Book = require('./models/book');
+
+mongoose.connect('mongodb://127.0.0.1:27017/friends-shelves');
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log('Database connected');
+});
+
+const app = express();
+
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+
+app.get('/', (req, res) => {
+    res.redirect('books/mine')
+});
+
+app.get('/books', async (req, res) => {
+    const books = await Book.find({});
+    res.render('books/all', { books });
+});
+
+app.get('/books/mine', (req, res) => {
+    res.render('books/mine')
+});
+
+app.get('/books/new', (req, res) => {
+    res.render('books/new');
+});
+
+app.post('/books', async (req, res, next) => {
+    const book = new Book(req.body.book);
+    await book.save();
+    res.redirect(`/books/${book._id}`);
+})
+
+app.get('/books/:id', async (req, res) => {
+    const book = await Book.findById(req.params.id)
+    // .populate({
+    //     path: 'reviews',
+    //     populate: {
+    //         path: 'author'
+    //     },
+    // }).populate('author');
+    // console.log(campground);
+    // if (!campground) {
+    //     req.flash('error', 'Cannot find that campground!');
+    //     return res.redirect('/campgrounds');
+    // };
+    res.render('books/show', { book });
+})
+
+app.get('/books/:id/edit', async (req, res) => {
+    const book = await Book.findById(req.params.id)
+    res.render('books/edit', { book });
+})
+
+app.put('/books/:id', async (req, res) => {
+    const { id } = req.params;
+    const book = await Book.findByIdAndUpdate(id, { ...req.body.book });
+    // req.flash('success', 'Successfully updated campground!');
+    res.redirect(`/books/${book._id}`)
+})
+
+app.delete('/books/:id', async (req, res) => {
+    const { id } = req.params;
+    await Book.findByIdAndDelete(id);
+    // req.flash('success', 'Successfully deleted a campground!');
+    res.redirect('/books');
+})
+
+app.get('/makebook', async (req, res) => {
+    const book = new Book({ title: 'Name of the wind', author: 'Patrick Rothuss' });
+    await book.save();
+    res.send(book)
+})
+
+
+
+app.route('/register')
+    .get((req, res) => {
+        res.render('users/register');
+    })
+
+app.route('/login')
+    .get((req, res) => {
+        res.render('users/login');
+    })
+
+
+
+
+
+app.listen(8080, () => {
+    console.log('Serving on port 8080')
+});
---
+++ seeds/index.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const { firstName, lastName, titleStart, titleEnd } = require('./seedHelpers');
+const Book = require('../models/book');
+
+mongoose.connect('mongodb://127.0.0.1:27017/friends-shelves');
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log('Database connected');
+});
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+    await Book.deleteMany({});
+    // for checking if documents are properly connected:
+    // const c = new Book({ title: 'purple field' });
+    // await c.save();
+    for (let i = 0; i < 50; i++) {
+        const book = new Book({
+            author: `${sample(firstName)} ${sample(lastName)}`,
+            title: `${sample(titleStart)} ${sample(titleEnd)}`,
+            isbn: '978-3-608-93800-5',
+            image: 'https://tse1.mm.bing.net/th?id=OIP.8LPxqTvPFvkgPax6lRcYPwHaKe&pid=Api',
+            blurb: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa nihil nisi sit sapiente, eligendi, vero totam vel sequi asperiores ipsa iusto esse quaerat cumque doloribus delectus amet sint id minus.',
+        });
+        await book.save();
+    }
+}
+
+seedDB()
+    .then(() => {
+        mongoose.connection.close();
+    });
+
