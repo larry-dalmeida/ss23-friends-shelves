6025215d32aa5ac60e63ee0eacef7db392cfa07c
esta3 <rauschert-e@web.de>
2023-08-24 17:44:08 +0200

    add review feature to books

--- models/book.js
+++ models/book.js
@@ -1,17 +1,34 @@
 const mongoose = require('mongoose');
-// const Review = require('./review');
+const Review = require('./review');
 const Schema = mongoose.Schema;
 
 const BookSchema = new Schema({
     title: String,
     author: String,
-    isbn: Number,
+    isbn: String,
     image: String,
     blurb: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review',
+        }
+    ]
     // owner: {
     //     type: Schema.Types.ObjectId,
     //     ref: 'User',
     // },
+});
+
+// delete the reviews in the reviews database when a book is deleted
+BookSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
 });
 
 
