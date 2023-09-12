4168daedf6843d2b9bbfb8f8205e53b0c8915d33
esta3 <rauschert-e@web.de>
2023-09-12 11:49:49 +0200

    comments during review - how to proceed

--- friendsshelvesreactconversion/src/components/NavBar.jsx
+++ friendsshelvesreactconversion/src/components/NavBar.jsx
@@ -4,29 +4,30 @@ import bookShelves from '../assets/images/bookshelves.png';
 import logout from '../assets/images/logout.png';
 
 
-function NavBar({handleFetchBooks, setShowLogin}){
+function NavBar({ handleFetchBooks, setShowLogin }) {
 
 
-        const handleShowAllBooks = () =>{
-                handleFetchBooks("all"); 
+        const handleShowAllBooks = () => {
+                handleFetchBooks("all");  // all is not used in the fetchBook function 
         };
-        
-        const handleShowMyBooks = () =>{
-                handleFetchBooks("mine");
-        };  
 
-        const handleLogout = () =>{
+        const handleShowMyBooks = () => {
+                handleFetchBooks("mine");
+        };
+
+        // Esther: there should be a function for this in App.jsx with a get request to http://localhost:8080/
+        const handleLogout = () => {
                 setShowLogin(true);
         };
-   
-   //Show navbar 
-    return(<div className= "navBar">
-            <img  id='logo' src = {bookImage} />
-            <a className="nav-link" onClick = {handleShowAllBooks}> <img  className = 'linkImage' src = {bookShelves} /> All Books</a>
-            <a className="nav-link" onClick = {handleShowMyBooks}> <img  className = 'linkImage' src = {bookImage} /> My Bookshelf</a>
-            <a className="nav-link" onClick = {handleLogout}> <img  className = 'linkImage' src = {logout} /> Logout</a>         
-    </div>)
-        
+
+        //Show navbar 
+        return (<div className="navBar">
+                <img id='logo' src={bookImage} />
+                <a className="nav-link" onClick={handleShowAllBooks}> <img className='linkImage' src={bookShelves} /> All Books</a>
+                <a className="nav-link" onClick={handleShowMyBooks}> <img className='linkImage' src={bookImage} /> My Bookshelf</a>
+                <a className="nav-link" onClick={handleLogout}> <img className='linkImage' src={logout} /> Logout</a>
+        </div>)
+
 }
 
 export default NavBar;
