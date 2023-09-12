8e7f221237a2c26493071686e608e410ebf922b2
esta3 <rauschert-e@web.de>
2023-09-12 15:40:12 +0200

    get React to accept the Backend Data structure

--- friendsshelvesreactconversion/src/components/BookList.jsx
+++ friendsshelvesreactconversion/src/components/BookList.jsx
@@ -1,29 +1,30 @@
 import BookShow from './BookShow'
 
-function BookList({books, searchBooks, onDelete, onEdit, onSearch, user, showBooks}){
+function BookList({ books, searchBooks, onDelete, onEdit, onSearch, user, showBooks }) {
 
     //render all books in books array
     let renderedBooks = books.map((book) => {
-        return <BookShow onEdit = {onEdit} onDelete = {onDelete} onSearch = {onSearch} key={book.id} book={book} user={user} />;
-    }); 
+        return <BookShow onEdit={onEdit} onDelete={onDelete} onSearch={onSearch} key={book._id} book={book} user={user} />;
+        //Esther: do you actually need to pass key - it is included in book I think?
+    });
 
     //render books that adhere to search query if there are any 
-    if(searchBooks.length > 0){
+    if (searchBooks.length > 0) {
         renderedBooks = searchBooks.map((book) => {
-            return <BookShow onEdit = {onEdit} onDelete = {onDelete} onSearch = {onSearch} key={book.id} book={book} user={user} />;
-        }); 
+            return <BookShow onEdit={onEdit} onDelete={onDelete} onSearch={onSearch} key={book._id} book={book} user={user} />;
+        });
     }
-    
+
     let pageTitle = 'Your Bookshelf';
 
-    if(showBooks === "all"){
+    if (showBooks === "all") {
         pageTitle = 'All Books';
     }
 
-    return(<div>
-            <div className = "pageTitle"><h2>{pageTitle}</h2></div>
-            <div className ="book-list">{renderedBooks}</div>
-        </div>
+    return (<div>
+        <div className="pageTitle"><h2>{pageTitle}</h2></div>
+        <div className="book-list">{renderedBooks}</div>
+    </div>
     )
 }
 
