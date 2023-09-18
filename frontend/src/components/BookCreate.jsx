c99e36c850a846e218d0780f8828d8b3759f2de9
esta3 <rauschert-e@web.de>
2023-09-18 22:32:28 +0200

    cleaning code after FE setup + two folder structure

---
+++ frontend/src/components/BookCreate.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import BookCreateForm from './BookCreateForm';
+
+
+function BookCreate({ user, onCreate }) {
+
+  //Handle submit from the bookcreate form
+  const handleSubmit = (title, author, ISBN, blurb) => {
+    onCreate(title, author, ISBN, blurb, user);
+    setShowForm(false);
+
+  };
+
+  //Handle showing the bookcreateform when button is pressed
+  const [showForm, setShowForm] = useState(false);
+
+  const handleShow = () => {
+    setShowForm(!showForm);
+  }
+
+  //Show button for the book creation form, when form is closed
+  let content = <div><button className="buttonBookCreate" onClick={handleShow}> Add a Book </button></div>
+
+  //if statement to call on bookcreateform
+  if (showForm) {
+    content = <BookCreateForm onSubmit={handleSubmit} onClick={handleShow} />;
+  }
+
+  return (<div>
+    {content}
+  </div>
+  )
+}
+
+export default BookCreate;
