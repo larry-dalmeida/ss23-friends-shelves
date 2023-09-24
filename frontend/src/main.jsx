06098e7eeef4c38864e2547195bc88aa5971423f
AliDriessen <alexander.sommers@gmail.com>
2023-09-24 18:13:10 +0200

    Added routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,13 +4,16 @@ import App from './App.jsx'
 import './index.css'
 import { Provider as BooksProvider } from './context/books';
 import { Provider as UserProvider } from './context/user';
+import { NavigationProvider } from './context/navigation';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <UserProvider>
-    <BooksProvider>
-      <React.StrictMode>
-        <App />    
-      </React.StrictMode>
-    </BooksProvider>
-  </UserProvider>,
+  <NavigationProvider>
+    <UserProvider>
+      <BooksProvider>
+        <React.StrictMode>
+          <App />    
+        </React.StrictMode>
+      </BooksProvider>
+    </UserProvider>
+  </NavigationProvider>,
 )
